3e388284b3836c433e0130ad8d538e506c1ffc27
Tarcio Teles <tarcioapps7@gmail.com>
2023-07-28 00:07:05 -0300

    add

--- Projeto-React/src/App.jsx
+++ Projeto-React/src/App.jsx
@@ -1,9 +1,29 @@
 
 
 function App() {
+  const nome = "Tarcio Teles";
+
+function retornaNome() {
+  return nome;
+}
+
+const pessoa = {
+  nome: 'Maria'
+}
+
+let estaDeDia = false;
 
   return (
+    <>
     <h1>Teste</h1>
+    <h2>Sub-Título</h2>
+    if or else
+    {estaDeDia ? 'Bom dia' : 'Boa noite'}
+    if
+    {estaDeDia && 'Bom dia'}
+
+
+    </>
   )
 }
 
